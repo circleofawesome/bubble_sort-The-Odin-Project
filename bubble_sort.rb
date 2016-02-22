852875e8f9d61b2861c3691ba510b46863884676
Samiul Anwar <samiul.anwar@gmail.com>
2016-02-22 11:52:56 -0500

    working as I think I understand the assignment

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -15,3 +15,22 @@ end
 
 bubble_sort([10,2,9,3,66,45,20,1])
 #everything works;completed
+
+
+def bubble_sort_by(strings=[])
+	length=strings.length
+	stop=length-2
+	length.times do
+		for i in 0..stop do
+			if yield(strings[i],strings[i+1])>0
+			 	temp=strings[i]
+			 	strings[i]=strings[i+1]
+			 	strings[i+1]=temp
+			end 
+		end	
+	end
+	return strings
+end
+
+bubble_sort_by(["hello","he"]) {|left,right|left.length-right.length}
+#works
